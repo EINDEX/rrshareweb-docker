9a73341adf012763a3efe4cf784c21635362a863
EINDEX <snowstarlbk@gmail.com>
2020-11-08 21:59:39 +0800

    support fork repo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM scratch
 WORKDIR /opt/work
-COPY rrshareweb /opt/work/rrshareweb
+COPY ./rrshareweb /opt/work/rrshareweb
 EXPOSE 3001
 
 CMD [ "/opt/work/rrshareweb/rrshareweb" ]
--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #bash 
 set -e
 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
 download_link=`wget -q -O - http://app.rrys.tv/ | grep -Eo "http\://appdown\.rrys\.tv/.*linux.*\.tar\.gz"`
 version=`echo $download_link |sed -E "s/.*rrshareweb_linux_(.*).tar.gz/\1/"`
 
 wget $download_link
 tar -xzf *linux*.tar.gz
 
-docker build . -t x86_64 -t latest -t linux -t linux_$version -t x86_64_$version
+docker build . -t rrshareweb -t $DOCKER_USERNAME/rrshareweb:x86_64 -t $DOCKER_USERNAME/rrshareweb:latest -t $DOCKER_USERNAME/rrshareweb:linux -t $DOCKER_USERNAME/rrshareweb:linux_$version -t $DOCKER_USERNAME/rrshareweb:x86_64_$version
+
+docker push $DOCKER_USERNAME/rrshareweb
